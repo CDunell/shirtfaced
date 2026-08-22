ab63c5a55bac21dddad73d0cf9c5988a8ca93320
CDunell <cdunell@gmail.com>
2026-08-23 00:13:15 +1000

    feat(curbstamps): match approved homepage section order

--- curbstamps-site/src/app/page.tsx
+++ curbstamps-site/src/app/page.tsx
@@ -1,8 +1,9 @@
 import { HeroWeirdo } from "@/components/home/HeroWeirdo";
 import { NewDropStrip } from "@/components/home/NewDropStrip";
-import { TrustStrip } from "@/components/home/TrustStrip";
 import { CurbCrewScene } from "@/components/home/CurbCrewScene";
+import { FindWeirdo } from "@/components/home/FindWeirdo";
 import { ShopByCategory } from "@/components/home/ShopByCategory";
+import { TrustStrip } from "@/components/home/TrustStrip";
 import { WeirdoMatch } from "@/components/home/WeirdoMatch";
 import { AdventureGrid } from "@/components/home/AdventureGrid";
 import { ParentsCorner } from "@/components/home/ParentsCorner";
@@ -14,11 +15,12 @@ export default function HomePage() {
       <HeroWeirdo />
       <NewDropStrip />
       <CurbCrewScene />
+      <FindWeirdo />
       <ShopByCategory />
+      <TrustStrip />
       <WeirdoMatch />
       <AdventureGrid />
       <ParentsCorner />
-      <TrustStrip />
       <NewsletterJoin />
     </div>
   );
